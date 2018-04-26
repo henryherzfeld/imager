8fc0168f73e7a53131467e179d7b608dc6b666fa
HenryHerzfeld <herzfeld2@gmail.com>
2018-04-26 15:27:38 -0400

    fixed route controller logic

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,7 @@ class PostController extends Controller
                 $post->IMAGE_PATH = $request->file('filename')->storeAs('public', $post->IMAGE_NAME);
                 $post->save();
                 $request->flash();
-                $url = env('APP_PATH')."/posts/create/{$post->id}/filter";
+                $url = "/posts/create/{$post->id}/filter";
                 return redirect($url);
             }
             else
@@ -81,7 +81,7 @@ class PostController extends Controller
              $post->save();
             }
             $request->flash();
-            $url = env('APP_PATH')."/posts/create/{$post->id}/caption";
+            $url = "/posts/create/{$post->id}/caption";
             return redirect($url);
         }
         else if($step === 'caption')
@@ -116,7 +116,7 @@ class PostController extends Controller
         DB::update('update users set postID = ? where  id = ?', [$post->id, Auth::user()->id]);
 
 
-        return redirect(env('APP_PATH')."posts/create/{$post->id}/upload");
+        return redirect("posts/create/{$post->id}/upload");
     }
 
     public function wall()
